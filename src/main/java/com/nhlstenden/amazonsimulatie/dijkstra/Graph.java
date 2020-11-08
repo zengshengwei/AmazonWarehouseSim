2aa62771e82e6e6447744141fd8bc0d100461777
Max Prakken <maxprakken3@hotmail.nl>
2020-11-08 17:25:42 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/nhlstenden/amazonsimulatie/dijkstra/Graph.java
+++ src/main/java/com/nhlstenden/amazonsimulatie/dijkstra/Graph.java
@@ -1,14 +1,14 @@
-package com.nhlstenden.amazonsimulatie.dijkstra;
+// package com.nhlstenden.amazonsimulatie.dijkstra;
 
-import java.util.*;
+// import java.util.*;
 
-public class Graph {
-    private Map<Node, List<Node>> neighbors;
+// public class Graph {
+//     private Map<Node, List<Node>> neighbors;
 
 
-    public void addNode(Node node){
-        neighbors.putIfAbsent(node, new ArrayList<Node>());
-    }
+//     public void addNode(Node node){
+//         neighbors.putIfAbsent(node, new ArrayList<Node>());
+//     }
 
     
-}
+// }
